18c9dbd7fd79083dbfb8ee1e7e8a6e0fe8b680f7
Roderick Fayson <rgfayson2@gmail.com>
2022-08-15 10:19:07 -0400

    first-antd-push

---
+++ src/components/Login.jsx
@@ -0,0 +1,28 @@
+import { Form, Input, Button } from 'antd'
+
+function Login() {
+    const handleForm = ({ email, password }) => {
+        alert('Form submitted with' + email + 'and' + password)
+    }
+    return (
+        <>
+        <h2>Login</h2>
+        <Form onFinish={handleForm} labelCol={{span:8}} wrapperCol={{ span:16}}>
+            <Form.Item label="Email" name="email" rules={[{
+                required:true,
+                message:'Please enter a valid email'
+            }]}>
+                <Input />
+            <Form.Item label="Password" name="password" rules={[{
+                required:true,
+                message:'Please enter a valid Password'
+            }]}>
+                <Input.Password />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">Submit</Button>
+            </Form>
+        </>
+    )
+}
+
+export default Login;
