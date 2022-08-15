407c6576ae73f4ef304093bd19e289207bf1edd7
Roderick Fayson <rgfayson2@gmail.com>
2022-08-15 11:02:48 -0400

    second push

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,21 +6,22 @@ function Login() {
     }
     return (
         <>
-        <h2>Login</h2>
-        <Form onFinish={handleForm} labelCol={{span:8}} wrapperCol={{ span:16}}>
-            <Form.Item label="Email" name="email" rules={[{
-                required:true,
-                message:'Please enter a valid email'
-            }]}>
-                <Input />
-            <Form.Item label="Password" name="password" rules={[{
-                required:true,
-                message:'Please enter a valid Password'
-            }]}>
-                <Input.Password />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">Submit</Button>
-            </Form>
+         <h2>Login</h2>
+      <Form onFinish={handleForm} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        <Form.Item label="Email" name="email" rules={[{
+          required: true,
+          message: 'Please enter a valid email'
+        }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{
+          required: true,
+          message: 'Please enter a password'
+        }]}>
+          <Input.Password />
+        </Form.Item>
+        <Button type="primary" htmlType='submit'>Submit</Button>
+      </Form>
         </>
     )
 }
